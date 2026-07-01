f0891f0e50165123e4b0c74d881ef93d87b4b51b
Vijay Nemishte <vijaynemishte80@gmail.com>
2026-07-01 12:45:56 +0530

    streamlit setup * homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+def footer_home():
+
+    
+    st.markdown(f"""
+                
+        <div  style="margin-top:5rem; display:flex;gap:6px; justify-content:center;" >
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by Vijay</p>
+
+        </div>       
+     
+    """,unsafe_allow_html=True)
